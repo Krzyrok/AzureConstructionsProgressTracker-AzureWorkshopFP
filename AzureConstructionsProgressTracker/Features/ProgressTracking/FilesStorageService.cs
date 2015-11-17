894b28601e8d039992198e20983323bdda200d70
Krzyrok <krzyrok@gmail.com>
2015-11-17 10:52:50 +0100

    adding files (attachmnets) to the blob by Progress Tracking

--- AzureConstructionsProgressTracker/Features/ProgressTracking/FilesStorageService.cs
+++ AzureConstructionsProgressTracker/Features/ProgressTracking/FilesStorageService.cs
@@ -1,7 +1,10 @@
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace AzureConstructionsProgressTracker.Features.ProgressTracking
 {
@@ -18,7 +21,28 @@ namespace AzureConstructionsProgressTracker.Features.ProgressTracking
             // - create container
             // - upload blob
             // - return URL
-            throw new NotImplementedException();
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=krzyrokazuretraining;AccountKey=WWHibxIfKtbQCQFMoMNVcouAPwNSTpcFF3b77PnyXQ7wVlf9DkGDRFfgul4qUxUKLLkPQrfnHw3r4sWgZWyNBg==");
+            
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve a reference to a container.
+            CloudBlobContainer container = blobClient.GetContainerReference("krzyrokazuretrainingblobcontainer");
+
+            // Create the container if it doesn't already exist.
+            if (container.CreateIfNotExists())
+            {
+                container.SetPermissions(
+                new BlobContainerPermissions
+                {
+                    PublicAccess = BlobContainerPublicAccessType.Blob
+                });
+            }
+
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
+            blockBlob.UploadFromStream(file.InputStream);
+
+            return blockBlob.Uri.AbsoluteUri;
         }
     }
 }
